942f35cb904eb69219580b1be854e1ffffaca4cd
podpah <stream_bars07@icloud.com>
2024-03-17 02:14:24 +0000

    Added TTL logic/flags

--- README.md
+++ README.md
@@ -5,20 +5,25 @@ Manga tracker that allows you to check for new chapter releases
 You can run the script through the CLI:
 ```console
 python main.py
-```
-You can also specify a usernarme if you want to use Anilist to get your list of currents by adding it after main.py or by specifying a default on line 11
+``` 
 
 Or through the .bat file (uses venv):
-```cmd
+```console
 runner.bat
 ```
 Helpful to run on start up if you want to keep up with new releases
 
-You can uncomment the code to check your Anilist profile for what you are currently reading, but this will get the names in Romanji which Mangapill doesn't always have, so I suggest running the bookmarklet.js or:
+You can add your username to check your Anilist profile (line 12 for default or add -u flag) for what you are currently reading, but this will get the names in Romanji which Mangapill doesn't always have, so I suggest running the bookmarklet.js or:
 ```js
 Array.from(document.querySelector('div.list-wrap').querySelectorAll('a'), a => a.textContent.trim())
 ```
 And updating the titles.txt file. The bookmarklet already copies the list to your titles and removes the array structure
+
+# Flags
+Flag | Fullhand | Param | Help
+--- | --- | --- | ---
+-u | --user |  Anilist username | Anilist username if you want to check releases against it (not recommended). You can also specify it on line 12 as a default
+-t | --ttl | No param | Decides whether to use TTL mode or not.  Default is yes but to change it, change store_false to store_true on line 13. TTL mode doesn't notify you of anything that has been found in the last 48 hours.
 
 # Limitations
 This only checks for the 120 latest releases on Mangapill, not if your manga has had new chapters
--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from bs4 import BeautifulSoup
-import requests
-# import sys
+from requests import get
+from argparse import ArgumentParser
+from json import dumps, loads, JSONDecodeError
+from datetime import datetime, timedelta
 
 # Array.from(document.querySelector('div.list-wrap').querySelectorAll('a'), a => a.textContent.trim())
 titles = set(open("titles.txt").read().lower().split("\n"))
+current_time = datetime.now()
+
+parser = ArgumentParser(description="Tracker for recently released manga chapters")
+parser.add_argument("-u","--user", help="Specifies Anilist user", default="", type=str)
+parser.add_argument("-t", "--ttl", help="Decides whether to use TTL mode or not", action='store_false')
+args = parser.parse_args()
 
 # You can use this but I recommend titles file instead as this will get Romanji names, which Mangapill doesn't have for everything
-# def get_user_reading():
-#    username = sys.argv[1] if len(sys.argv) > 1 else "user"
-#    res = requests.get(f"https://anilist.co/user/{username}/mangalist")
-#    soup = BeautifulSoup(res.text, "html.parser")
-#    reading = soup.find("div", class_="list-wrap")
-#    return {title.text.strip() for title in reading.find_all("div", class_="title")}[1:] # Returns all but first result as that just says title
+def get_user_reading():
+   res = get(f"https://anilist.co/user/{args.user}/mangalist")
+   soup = BeautifulSoup(res.text, "html.parser")
+   reading = soup.find("div", class_="list-wrap")
+   # Returns all but first result as that just says title
+   return {title.text.strip() for i, title in enumerate(reading.find_all("div", class_="title")) if i != 0}
 
 
 def get_mangapill_recents():
-    res = requests.get("https://mangapill.com/chapters")
+    res = get("https://mangapill.com/chapters")
     soup = BeautifulSoup(res.text, "html.parser")
     divs = soup.find("div", class_="grid").find_all("a", class_="text-secondary")
     recents = set()
@@ -24,10 +32,43 @@ def get_mangapill_recents():
     return recents
 
 
+def update_recently_found(recently_found: list, intersection):
+    recently_found += [[title, current_time.strftime('%Y-%m-%d %H:%M:%S')] for title in intersection]
+
+    with open('recents.json','w') as file:
+        file.write(dumps(recently_found))
+
+
+def get_recently_found():
+    with open('recents.json') as file:
+        try:
+            recently_found = loads(file.read())
+        except JSONDecodeError:
+            recently_found = []
+    mangapill = get_mangapill_recents()
+
+    for index, each in reversed(list(enumerate(recently_found))):
+        if current_time - datetime.strptime(each[1], '%Y-%m-%d %H:%M:%S') >= timedelta(hours=48):
+            recently_found.pop(index)
+        elif each[0] in mangapill:
+            mangapill.remove(each[0])
+
+    intersection = titles.intersection(mangapill)
+
+    update_recently_found(recently_found, intersection)
+
+    print("New releases: " + ", ".join([each.title() for each in intersection]) if intersection else "No elements")
+
+
 def comparison():
-    intersection = titles.intersection(get_mangapill_recents())
-    # intersection = get_user_reading().intersection(get_mangapill_recents())
-    return str("New releases: " + ", ".join([each.title() for each in intersection])) if intersection else "No elements"
+    if args.user:
+        intersection = get_user_reading().intersection(get_mangapill_recents())
+    else:
+        intersection = titles.intersection(get_mangapill_recents())
+    print("New releases: " + ", ".join([each.title() for each in intersection]) if intersection else "No elements")
 
 
-print(comparison())
+if args.ttl:
+    get_recently_found()
+else:
+    comparison()
